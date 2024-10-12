a088653c451c177f00c38008cc12e77d2388a2fb
Mohamed Bahgat <52916078+mobahagt123@users.noreply.github.com>
2024-10-12 11:23:27 +0300

    Add files via upload

--- fetch-data.js
+++ fetch-data.js
@@ -3,12 +3,12 @@ async function fetchUserData() {
 
     const apiUrl = 'https://jsonplaceholder.typicode.com/users'
     
-    let DataContainer = document.getElementById('api-data')
+    let dataContainer = document.getElementById('api-data')
     
     try {
         const response = await fetch(apiUrl)
         const users = await response.json()
-        DataContainer.innerHTML  = ''
+        dataContainer.innerHTML  = ''
         const userList = document.createElement('ul')
 
         users.forEach((user) => {
@@ -18,13 +18,13 @@ async function fetchUserData() {
             userList.appendChild(list)    
         });
 
-        DataContainer.appendChild(userList)
+        dataContainer.appendChild(userList)
     
     }
     catch(error){
         error = 'Failed to load user data.'
-        DataContainer.innerHTML = ''
-        DataContainer.textContent = error
+        dataContainer.innerHTML = ''
+        dataContainer.textContent = error
     
     }
         
